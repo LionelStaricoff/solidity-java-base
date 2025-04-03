0b1af48bcbce724bc4380c9736d10e11646a8492
lionel staricoff <staricofflionel@hotmail.com>
2025-04-03 16:04:58 -0500

    feat: first commit dependencies

---
+++ src/main/java/com/openlab/OpenlabApplication.java
@@ -0,0 +1,13 @@
+package com.openlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlabApplication.class, args);
+	}
+
+}
