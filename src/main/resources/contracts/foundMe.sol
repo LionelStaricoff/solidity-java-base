7d4a3c2505f451e4d7694898ce1c4fda64755d1b
lionel staricoff <staricofflionel@hotmail.com>
2025-04-12 03:11:33 -0500

    feat:compilanod pero no encuentra los contratos aun

--- src/main/resources/contracts/foundMe.sol
+++ src/main/resources/contracts/foundMe.sol
@@ -2,7 +2,9 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
 
 contract founMe {
     AggregatorV3Interface internal dataFeed;
